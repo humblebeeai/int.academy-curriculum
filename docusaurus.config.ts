b2a1f50e0d6e0f06342f96c6c18c8e247a45245f
abduvaliy-hbai <abduvaliy.hbai@gmail.com>
2026-04-04 09:39:42 +0500

    chore(release): prepare v1.0.0
    Bump package version to 1.0.0 and document release notes in the changelog.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,9 +10,9 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
+  // future: {
+  //   v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  // },
 
 // The compatibility with Docusaurus v4 is currently being tested, but enabling the future flag causes some issues with the current version. We will enable it once we are ready to migrate to v4.
 
